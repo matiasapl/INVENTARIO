63c9e5f17dd4231f54c4ee3a8301f4641d365811
matiasapl <polhwein.matias@gmail.com>
2025-08-01 13:38:30 -0400

    ahora mysql guarda el hash sha256 y ya no el binario del hash todas las sesiones pasan a manejarse por hash directo

--- PROYECTO_INVENTARIO/src/MVC/MODELO/DDBB/RegistrarUsuario.php
+++ PROYECTO_INVENTARIO/src/MVC/MODELO/DDBB/RegistrarUsuario.php
@@ -8,7 +8,7 @@ if ($_SERVER['REQUEST_METHOD'] === 'POST') {
      if (isset($_POST['username']) && isset($_POST['email']) && isset($_POST['password'])) {
         $USERNAME = $_POST['username'];
         $EMAIL = $_POST['email'];
-        $PASS = hash('sha256', $_POST['password'], true); // HASHEA CONTRASEÑA Y GUARDA LA CONTRASEÑA EN BINARIO
+        $PASS = hash('sha256', $_POST['password']); // HASHEA CONTRASEÑA
         
         //verifica que el correo electronico no se encuentre registrado
         $stmt = $conn->prepare("SELECT EMAIL FROM USUARIOS WHERE EMAIL = ?");
@@ -26,8 +26,7 @@ if ($_SERVER['REQUEST_METHOD'] === 'POST') {
         $stmt->close(); // en caso de que no este registrado cierra stmt y contunia al proceso para registrar al usuario
         $null = null;
         $stmt = $conn->prepare("INSERT INTO USUARIOS (PASS, EMAIL, USERNAME) VALUES (?, ?, ?)"); //PREPARA LA CONSULTA
-        $stmt->bind_param("bss", $null, $EMAIL, $USERNAME); // "b" para BLOB (requiere manejo especial)
-        $stmt->send_long_data(0, $PASS); // posición 0 = primer parámetro ("b")
+        $stmt->bind_param("sss", $PASS, $EMAIL, $USERNAME); // "b" para BLOB (requiere manejo especial)
         $stmt->execute(); // EJECUTA LA CONSULTA
         $stmt->close(); // CIERRA LA CONSULTA
         $conn->close(); // CIERRA LA CONEXIÓN
--- PROYECTO_INVENTARIO/src/MVC/MODELO/DDBB/VerificarUsuario.php
+++ PROYECTO_INVENTARIO/src/MVC/MODELO/DDBB/VerificarUsuario.php
@@ -1,6 +1,5 @@
 <?php
     require_once 'Conexion.php';
-
 ?>
 
 <?php
@@ -11,11 +10,10 @@ if ($_SERVER['REQUEST_METHOD'] === 'POST') { // verifica que sea una solicitud P
     $username = $_POST['username']; // almacena datos en variables
     $password = $_POST['password'];
 
-    $hash = hash('sha256', $password, true); // hashea y convierte la contraseña a un formato binario
+    $hash = hash('sha256', $password); // hashea la contraseña
 
     $stmt = $conn->prepare("SELECT ID, USERNAME, EMAIL FROM USUARIOS WHERE USERNAME = ? AND PASS = ?"); // prepara la consulta SQL para evitar inyecciones SQL
-    $stmt->bind_param("sb", $username, $null); // vincula los parámetros a la consulta preparada
-    $stmt->send_long_data(1, $hash); // agrega el hash de la contraseña como un dato largo reemplazando el segundo parámetro
+    $stmt->bind_param("ss", $username, $hash); // vincula los parámetros a la consulta preparada
 
     $stmt->execute(); // ejecuta la consulta preparada
     $resultado = $stmt->get_result(); // obtiene el resultado de la consulta
