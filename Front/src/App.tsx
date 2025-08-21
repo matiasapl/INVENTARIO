1fdff1f19f701d7ec8a866899d142f030fc61974
matiasapl <polhwein.matias@gmail.com>
2025-08-21 18:12:37 -0400

    un par de pequeños cambios

--- Front/src/App.tsx
+++ Front/src/App.tsx
@@ -1,11 +1,16 @@
 import { useState } from "react";
-
+import "/node_modules/bootstrap/dist/css/bootstrap.min.css";
 import "./App.css";
+import Login from "./components/Login/Index";
 
 function App() {
   const [count, setCount] = useState(0);
 
-  return <></>;
+  return (
+    <>
+      <Login />
+    </>
+  );
 }
 
 export default App;
