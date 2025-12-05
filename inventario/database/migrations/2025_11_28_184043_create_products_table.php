d5a4d9900f5b53e38ef6e9046d4ca4d2572d61fd
matiasapl <polhwein.matias@gmail.com>
2025-12-05 17:34:59 -0300

    correccion tipos de datos y htaccess para produccion

--- inventario/database/migrations/2025_11_28_184043_create_products_table.php
+++ inventario/database/migrations/2025_11_28_184043_create_products_table.php
@@ -18,8 +18,8 @@ return new class extends Migration
         $table->string('nombre');
         $table->string('descripcion')->nullable();
         $table->unsignedInteger('stock')->default(0);
-        $table->unsignedDecimal('precio_unitario', 10, 2)->default(0.00);
-        $table->unsignedDecimal('M3_unitario', 14, 5)->default(0.00000);
+        $table->decimal('precio_unitario', 10, 2)->unsigned()->default(0.00);
+        $table->decimal('M3_unitario', 14, 5)->unsigned()->default(0.00000);
         $table->foreignId('usuario')->constrained('users')->onDelete('cascade');
         $table->timestamps();
     });
