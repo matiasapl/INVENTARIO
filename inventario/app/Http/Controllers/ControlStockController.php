c05d6799c99182b43ea70285abb5aabfb089fc21
matiasapl <polhwein.matias@gmail.com>
2025-12-27 20:57:08 -0300

    creado make:model control_stock --all pendiente dirseñar migraciones, controlador model etc

---
+++ inventario/app/Http/Controllers/ControlStockController.php
@@ -0,0 +1,66 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use App\Models\control_stock;
+use App\Http\Requests\Storecontrol_stockRequest;
+use App\Http\Requests\Updatecontrol_stockRequest;
+
+class ControlStockController extends Controller
+{
+    /**
+     * Display a listing of the resource.
+     */
+    public function index()
+    {
+        //
+    }
+
+    /**
+     * Show the form for creating a new resource.
+     */
+    public function create()
+    {
+        //
+    }
+
+    /**
+     * Store a newly created resource in storage.
+     */
+    public function store(Storecontrol_stockRequest $request)
+    {
+        //
+    }
+
+    /**
+     * Display the specified resource.
+     */
+    public function show(control_stock $control_stock)
+    {
+        //
+    }
+
+    /**
+     * Show the form for editing the specified resource.
+     */
+    public function edit(control_stock $control_stock)
+    {
+        //
+    }
+
+    /**
+     * Update the specified resource in storage.
+     */
+    public function update(Updatecontrol_stockRequest $request, control_stock $control_stock)
+    {
+        //
+    }
+
+    /**
+     * Remove the specified resource from storage.
+     */
+    public function destroy(control_stock $control_stock)
+    {
+        //
+    }
+}
