33e27a7b30aaabf423b1339c5e07de59512139d8
Matias Alexander polhwein Lara <polhwein.matias@gmail.com>
2026-04-19 18:27:50 -0400

    refactor: :construction: Ordenadas Rutas de web.php

--- inventario/routes/web.php
+++ inventario/routes/web.php
@@ -16,52 +16,19 @@ Route::get('/', function () {
 })->name('home');
 
 
-//Dashboard
+
 Route::middleware(['auth', 'verified'])->group(function () {
+    
+    //--------Inicio Secciones de la APP--------
     Route::get('dashboard',  [ProductController::class, 'dashboard'])->name('dashboard');
-});
-
-
-Route::middleware(['auth'])->group(function () {
-
-
-    //Lotes
-    Route::get('/lotes', [LotesController::class, 'index'])->name('lotes.index');
-    Route::get('/lotes/papelera', [LotesController::class, 'papelera'])->name('lotes.papelera');
-    Route::get('/lotes/{Lote}/deshabilitar', [LotesController::class, 'deshabilitar'])->name('lotes.deshabilitar');
-    Route::get('/lotes/{Lote}/habilitar', [LotesController::class, 'habilitar'])->name('lotes.habilitar');
-    Route::delete('/lotes/{Lote}/eliminar', [LotesController::class, 'eliminar'])->name('lotes.eliminar');
-    Route::get('/lotes/create', [LotesController::class, 'create'])->name('lotes.create');
-    Route::get('/lotes/edit/{lote}', [LotesController::class, 'edit'])->name('lotes.edit');
-    Route::get('/lotes/view/{lote}', [LotesController::class, 'view'])->name('lotes.view');
-    Route::post('/lotes/store', [LotesController::class, 'store'])->name('lotes.store');
-    // Secciones
-
-
-    Route::get('/productos', [ProductController::class, 'index'])->name('products.index');
-    Route::get('/registros', [RegistroController::class, 'index'])->name('registros.index');
-
-    Route::get('/almacenes', [AlmacenController::class, 'index'])->name('almacenes.index');
-    Route::get('/almacenes/create', [AlmacenController::class, 'create'])->name('almacenes.create');
-    Route::post('/almacenes/store', [AlmacenController::class, 'store'])->name('almacen.store');
-    Route::get('/almacenes/papelera', [AlmacenController::class, 'papelera'])->name('almacen.papelera');
-    Route::get('/almacenes/{almacen}/deshabilitar', [AlmacenController::class, 'deshabilitar'])->name('almacen.deshabilitar');
-    Route::get('/almacenes/edit/{almacen}', [AlmacenController::class, 'edit'])->name('almacen.edit');
-    Route::put('/almacenes/update/{almacen}', [AlmacenController::class, 'update'])->name('almacen.update');
-    //Papelera Almacenes
-    Route::get('/almacenes/{almacen}/habilitar', [AlmacenController::class, 'habilitar'])->name('almacen.habilitar');
-    Route::delete('/almacenes/{almacen}/eliminar', [AlmacenController::class, 'eliminar'])->name('almacen.eliminar');
-    Route::get('/almacenes/view/{almacen}', [AlmacenController::class, 'view'])->name('almacen.view');
-
+    Route::get('productos', [ProductController::class, 'index'])->name('products.index');
+    Route::get('almacenes', [AlmacenController::class, 'index'])->name('almacenes.index');
+    Route::get('lotes', [LotesController::class, 'index'])->name('lotes.index');
+    Route::get('registros', [RegistroController::class, 'index'])->name('registros.index');
     
-    
-    
-    //Productos Crear
-    Route::post('/productos/store', [ProductController::class, 'store'])->name('products.store');
+    //--------Fin Secciones de la APP--------
 
-    //Productos Editar
-    Route::put('/productos/update/{product}', [ProductController::class, 'update'])->name('products.update');
-
+    //--------Inicio Productos--------
     //Productos Index
     Route::get('/productos/create', [ProductController::class, 'create'])->name('products.create');
     Route::get('/productos/papelera', [ProductController::class, 'papelera'])->name('products.papelera');
@@ -69,12 +36,61 @@ Route::middleware(['auth'])->group(function () {
     Route::get('/productos/view/{product}', [ProductController::class, 'view'])->name('products.view');
     Route::get('/productos/{product}/deshabilitar', [ProductController::class, 'deshabilitar'])->name('products.deshabilitar');
 
-    //Papelera Productos
+    //Productos Papelera 
     Route::get('/productos/{product}/habilitar', [ProductController::class, 'habilitar'])->name('products.habilitar');
     Route::delete('/productos/{product}/eliminar', [ProductController::class, 'eliminar'])->name('products.eliminar');
-});
+
+    //Crear Productos 
+    Route::post('/productos/store', [ProductController::class, 'store'])->name('products.store');
+
+    //Editar Productos 
+    Route::put('/productos/update/{product}', [ProductController::class, 'update'])->name('products.update');
+
+    //--------Fin Productos--------
+
+    //--------Inicio Almacenes--------
+    //Almacenes Index
+    Route::get('/almacenes/create', [AlmacenController::class, 'create'])->name('almacenes.create');
+    Route::get('/almacenes/papelera', [AlmacenController::class, 'papelera'])->name('almacen.papelera');
+    Route::get('/almacenes/edit/{almacen}', [AlmacenController::class, 'edit'])->name('almacen.edit');
+    Route::get('/almacenes/view/{almacen}', [AlmacenController::class, 'view'])->name('almacen.view');
+    Route::get('/almacenes/{almacen}/deshabilitar', [AlmacenController::class, 'deshabilitar'])->name('almacen.deshabilitar');
+    
+    //Almacenes Papelera 
+    Route::get('/almacenes/{almacen}/habilitar', [AlmacenController::class, 'habilitar'])->name('almacen.habilitar');
+    Route::delete('/almacenes/{almacen}/eliminar', [AlmacenController::class, 'eliminar'])->name('almacen.eliminar');
+
+    //Almacenes Crear
+    Route::post('/almacenes/store', [AlmacenController::class, 'store'])->name('almacen.store');
+
+    //Almacenes Editar
+    Route::put('/almacenes/update/{almacen}', [AlmacenController::class, 'update'])->name('almacen.update');
+
+    //--------Fin Almacenes--------
 
 
+
+
+    //--------Inicio Lotes--------
+    //Lotes Index
+    Route::get('/lotes/create', [LotesController::class, 'create'])->name('lotes.create');
+    Route::get('/lotes/papelera', [LotesController::class, 'papelera'])->name('lotes.papelera');
+    Route::get('/lotes/edit/{lote}', [LotesController::class, 'edit'])->name('lotes.edit');
+    Route::get('/lotes/view/{lote}', [LotesController::class, 'view'])->name('lotes.view');
+    Route::get('/lotes/{Lote}/deshabilitar', [LotesController::class, 'deshabilitar'])->name('lotes.deshabilitar');
+    
+    //Lotes Papelera
+    Route::get('/lotes/{Lote}/habilitar', [LotesController::class, 'habilitar'])->name('lotes.habilitar');
+    Route::delete('/lotes/{Lote}/eliminar', [LotesController::class, 'eliminar'])->name('lotes.eliminar');
+
+    //Lotes Crear
+    Route::post('/lotes/store', [LotesController::class, 'store'])->name('lotes.store');
+    
+    //Lotes Editar
+    Route::put('/lotes/update/{lote}', [LotesController::class, 'update'])->name('lotes.update');
+
+    //--------Fin Lotes--------
+    });
 require __DIR__.'/settings.php';
 
 
